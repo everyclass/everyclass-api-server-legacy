f75258b276aee2d9ca0ca959cb0b28a8e6eed8e4
Frederic <frederic.t.chan@gmail.com>
2019-01-20 10:43:00 +0800

    test version for disable logstash

--- everyclass/api_server/__init__.py
+++ everyclass/api_server/__init__.py
@@ -23,7 +23,7 @@ try:
     @uwsgidecorators.postfork
     def enable_gc():
         """enable garbage collection"""
-        # gc.set_threshold(700)
+        gc.set_threshold(700)
 
     @uwsgidecorators.postfork
     def init_db():
@@ -57,15 +57,15 @@ try:
             logger.info('You are in {} mode, so APM is inited.'.format(__app.config['CONFIG_NAME']))
 
         # Logstash centralized log
-        if __app.config['CONFIG_NAME'] in __app.config['LOGSTASH_AVAILABLE_IN']:
-            logstash_handler = LogstashHandler(host=__app.config['LOGSTASH']['HOST'],
-                                               port=__app.config['LOGSTASH']['PORT'],
-                                               release=__app.config['GIT_DESCRIBE'],
-                                               bubble=True,
-                                               logger=logger,
-                                               filter=lambda r, h: r.level >= 11)  # do not send DEBUG
-            logger.handlers.append(logstash_handler)
-            logger.info('You are in {} mode, so LogstashHandler is inited.'.format(__app.config['CONFIG_NAME']))
+        # if __app.config['CONFIG_NAME'] in __app.config['LOGSTASH_AVAILABLE_IN']:
+        #     logstash_handler = LogstashHandler(host=__app.config['LOGSTASH']['HOST'],
+        #                                        port=__app.config['LOGSTASH']['PORT'],
+        #                                        release=__app.config['GIT_DESCRIBE'],
+        #                                        bubble=True,
+        #                                        logger=logger,
+        #                                        filter=lambda r, h: r.level >= 11)  # do not send DEBUG
+        #     logger.handlers.append(logstash_handler)
+        #     logger.info('You are in {} mode, so LogstashHandler is inited.'.format(__app.config['CONFIG_NAME']))
 
         import uwsgi
         if uwsgi.worker_id() == 1 and __first_spawn:
